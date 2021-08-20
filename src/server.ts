ab164e5e02ad4d26c8a97e1e94bbbf8e630ff267
Kam Lasater <kam@cyclic.sh>
2021-08-19 20:01:05 -0400

    Drop back to vanilla js

--- src/server.ts
+++
@@ -1,7 +0,0 @@
-const {app} = require('./express-app')
-
-const port = process.env.PORT || 3000
-
-app.listen(port, () => {
-  console.log(`[cyclic] server on http://localhost:${port}`)
-})
--- test-app/api/index.js
+++ test-app/api/index.js
@@ -1,7 +1,7 @@
 module.exports.get = (req, res) => {
   // console.log('called /index.js:get()\nreq: ',req,'\nres: ',res)
   res.status = 200
-  res.headers['Content-Type'] = "text/html"
+  res.header('Content-Type','text/html')
   res.body = '<meta charset="UTF-8"><h1>Hello world!</h1>'
   return res
 }
